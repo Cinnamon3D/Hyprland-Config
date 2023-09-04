4ed8ec051ffbed499e029a612c9f5e0bf3c71704
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-09-04 16:06:30 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+su
+pacman -Syu
+
+#Install main GUI programs
+pacman -S alacritty wofi thunar flatpak zip neofetch blueman
+
+#Find a pulkit, wallpaper setter, gtk theme selector, btop, breeze-cursor-theme, topbar, volume and brightness, screen lock, 
+
+systemctl enable bluetooth
+systemctl start bluetooth
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+
+mv "hyprland.conf" "$HOME/.config/hypr/hyprland"
