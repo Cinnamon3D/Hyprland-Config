b34b43b7c87e6de231c9710048f3f9f0cd878625
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-09-13 07:48:26 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo su
 zypper update
 
 #Install main GUI programs
-zypper install google-chrome-stable hyprland kitty wofi thunar blueman nwg-look emacs breeze-cursor-theme 
+zypper install google-chrome-stable hyprland kitty wofi thunar blueman emacs breeze5-cursors
 
 #Install main CLI programs
 zypper install flatpak zip unzip neofetch bluez swaybg btop polkit-gnome pamixer brightnessctl dunst pipewire xdg-desktop-portal-hyprland
@@ -13,3 +13,4 @@ systemctl enable bluetooth
 systemctl start bluetooth
 
 mv hyprland.conf ~/.config/hypr/
+mv Wallpaper.jpg ~/.config/hypr/
