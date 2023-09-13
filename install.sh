9d47359a2273bcebf66372a6ac555fd23f56bee7
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-09-12 20:53:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
-su
-pacman -Syu
+sudo su
+zypper update
 
 #Install main GUI programs
-pacman -S alacritty wofi thunar flatpak zip unzip neofetch blueman emacs
+zypper install hyprland alacritty wofi thunar flatpak zip unzip neofetch blueman emacs
 
 #Find a pulkit, wallpaper setter, gtk theme selector, btop, breeze-cursor-theme, topbar, volume and brightness, screen lock, 
 
 systemctl enable bluetooth
 systemctl start bluetooth
 
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
-yay -S google-chrome
-yay -S nwg-look swaybg
+zypper install google-chrome
+zypper install nwg-look swaybg
 
 mv "hyprland.conf" "$HOME/.config/hypr/"
