989993ce8e0b67e84725509b2c7dc08ac853f6d3
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-09-13 08:39:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,23 @@ sudo su
 zypper update
 
 #Install main GUI programs
-zypper install google-chrome-stable hyprland kitty wofi thunar blueman emacs breeze5-cursors
+zypper install hyprland kitty wofi thunar blueman emacs breeze5-cursors
 
 #Install main CLI programs
-zypper install flatpak zip unzip neofetch bluez swaybg btop polkit-gnome pamixer brightnessctl dunst pipewire xdg-desktop-portal-hyprland
+zypper install flatpak zip unzip neofetch bluez swaybg btop polkit-gnome pamixer brightnessctl dunst pipewire xdg-desktop-portal-hyprland network-manager
 
-#Find a pulkit, volume and brightness, screen lock,
+# Install Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub > linux_signing_key.pub
+sudo rpm --import linux_signing_key.pub
+sudo zypper addrepo http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome
+sudo zypper refresh
+sudo zypper install google-chrome-stable
 
-systemctl enable bluetooth
-systemctl start bluetooth
+systemctl enable Bluetooth
+systemctl start Bluetooth
+
+systemctl enable NetworkManager
+systemctl start NetworkManager
 
 mv hyprland.conf ~/.config/hypr/
 mv Wallpaper.jpg ~/.config/hypr/
