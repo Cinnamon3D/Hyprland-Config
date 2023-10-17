a573e45230730c13308ec5cf049878bb4b9a463d
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-10-17 10:43:21 -0500

    Update freebsd-install.sh

--- freebsd-install.sh
+++ freebsd-install.sh
@@ -3,11 +3,18 @@
 # Sign into user account and install git with ("sudo pkg install git") the clone this repo with ("git clone https://github.com/SimeonBlack/Hyprland-Config")
 # Make script excicutible with ("chmod +x" freebsd-install.sh") then run the script with ("bash ./freebsd-install.sh")
 
+#_____________________________________________________________________________
+
+echo "Installing Hyprland and needed packages..."
 sudo pkg install kitty hyprland xdg-desktop-portal-hyprland drm-kmod seatd
+
+echo "Enabling Services..."
 export XDG_RUNTIME_DIR=/var/run/user/`id -u`
 sysrc seatd_enable=”YES”
 service seatd start
 pw groupmod video -m cinnamon
+
+#_____________________________________________________________________________
 
 echo "Would you like to install optional packages? (yes/no)"
 read response
@@ -20,3 +27,20 @@ elif [[ "$response" == "no" ]]; then
 else
     echo "Invalid response. Please enter 'yes' or 'no'"
 fi
+
+#_____________________________________________________________________________
+
+echo "Would you like to install other tools? (yes/no)"
+read response
+
+if [[ "$response" == "yes" ]]; then
+    echo "Installing optional packages..."
+    sudo pkg install python
+elif [[ "$response" == "no" ]]; then
+    echo "Okay, skipping..."
+else
+    echo "Invalid response. Please enter 'yes' or 'no'"
+fi
+
+#_____________________________________________________________________________
+
