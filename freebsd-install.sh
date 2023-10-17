20dae7f207be5e2788a29e793ea205c461aa7e2a
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-10-17 10:32:20 -0500

    Update freebsd-install.sh

--- freebsd-install.sh
+++ freebsd-install.sh
@@ -1,8 +1,23 @@
+#!/bin/bash
 # Sign into root account and insall pkg and sudo with ("pkg install sudo") add user to the sudo group with ("visudo") type ("username ALL=(ALL:ALL) ALL")
-#
-# Make script excicutible with ("chmod +x" freebsd-install.sh")
+# Sign into user account and install git with ("sudo pkg install git") the clone this repo with ("git clone https://github.com/SimeonBlack/Hyprland-Config")
+# Make script excicutible with ("chmod +x" freebsd-install.sh") then run the script with ("bash ./freebsd-install.sh")
+
 sudo pkg install hyprland xdg-desktop-portal-hyprland drm-kmod seatd
 export XDG_RUNTIME_DIR=/var/run/user/`id -u`
 sysrc seatd_enable=”YES”
 service seatd start
 pw groupmod video -m cinnamon
+
+echo "Do you want to proceed? (yes/no)"
+read response
+
+if [[ "$response" == "yes" || "$response" == "YES" ]]; then
+    echo "You chose to proceed."
+    # Place your code to execute when the answer is yes here
+elif [[ "$response" == "no" || "$response" == "NO" ]]; then
+    echo "You chose not to proceed."
+    # Place your code to execute when the answer is no here
+else
+    echo "Invalid response. Please enter 'yes' or 'no'."
+fi
