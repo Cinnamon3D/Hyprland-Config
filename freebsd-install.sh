74bc34304b0c81ec1175bf22e2d0b7e811b319d7
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-10-17 10:27:06 -0500

    Update freebsd-install.sh

--- freebsd-install.sh
+++ freebsd-install.sh
@@ -1,4 +1,6 @@
-
+# Sign into root account and insall pkg and sudo with ("pkg install sudo") add user to the sudo group with ("visudo") type ("username ALL=(ALL:ALL) ALL")
+#
+# Make script excicutible with ("chmod +x" freebsd-install.sh")
 sudo pkg install hyprland xdg-desktop-portal-hyprland drm-kmod seatd
 export XDG_RUNTIME_DIR=/var/run/user/`id -u`
 sysrc seatd_enable=”YES”
