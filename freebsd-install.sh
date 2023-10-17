7dbf8660a7d00e7ac725733069689e5e438ea291
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-10-17 10:38:48 -0500

    Update freebsd-install.sh

--- freebsd-install.sh
+++ freebsd-install.sh
@@ -3,21 +3,20 @@
 # Sign into user account and install git with ("sudo pkg install git") the clone this repo with ("git clone https://github.com/SimeonBlack/Hyprland-Config")
 # Make script excicutible with ("chmod +x" freebsd-install.sh") then run the script with ("bash ./freebsd-install.sh")
 
-sudo pkg install hyprland xdg-desktop-portal-hyprland drm-kmod seatd
+sudo pkg install kitty hyprland xdg-desktop-portal-hyprland drm-kmod seatd
 export XDG_RUNTIME_DIR=/var/run/user/`id -u`
 sysrc seatd_enable=”YES”
 service seatd start
 pw groupmod video -m cinnamon
 
-echo "Do you want to proceed? (yes/no)"
+echo "Would you like to install optional packages? (yes/no)"
 read response
 
-if [[ "$response" == "yes" || "$response" == "YES" ]]; then
-    echo "You chose to proceed."
-    # Place your code to execute when the answer is yes here
-elif [[ "$response" == "no" || "$response" == "NO" ]]; then
-    echo "You chose not to proceed."
-    # Place your code to execute when the answer is no here
+if [[ "$response" == "yes" ]]; then
+    echo "Installing optional packages..."
+    sudo pkg install ranger btop neofetch
+elif [[ "$response" == "no" ]]; then
+    echo "Okay, skipping..."
 else
-    echo "Invalid response. Please enter 'yes' or 'no'."
+    echo "Invalid response. Please enter 'yes' or 'no'"
 fi
